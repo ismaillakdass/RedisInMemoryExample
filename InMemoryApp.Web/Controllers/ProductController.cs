52c01d60fd50cabdda837f3d85b49f6d454e6fb1
ismaillakdass <ismaillakdass@gmail.com>
2023-07-20 23:36:12 +0300

    açıklamalar ve cache options

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -15,14 +15,22 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Index()
         {
             MemoryCacheEntryOptions cacheOptions = new MemoryCacheEntryOptions();
-            cacheOptions.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
-            _memoryCache.Set<string>("dateInMem", DateTime.Now.ToString(),cacheOptions);
+
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // Mutlak zaman değeri ile cache de kalma süresi belirtiliyor
+            cacheOptions.SlidingExpiration = TimeSpan.FromSeconds(2); // her istek geldiğinde ccach süresini 2 sn uzatıyor fakat absolute süresi varsa ve dolmuşsa yine siler
+
+            cacheOptions.Priority = CacheItemPriority.Normal; // Cache in önem derecesini belirtir ve ona göre hangi öncelik varsa o sıraya göre siler neverRemove hiç silmez fakat chach dolarsa exep fırlatır
+
+            
+            _memoryCache.Set<string>("dateInMem", DateTime.Now.ToString(),cacheOptions); // Cache oluşturuluyor
+
+
             return View();
         }
 
         public IActionResult Show()
         {
-            ViewBag.dateInMem = _memoryCache.Get<string>("dateInMem");
+            ViewBag.dateInMem = _memoryCache.Get<string>("dateInMem");// memory den belirtlen key e bağlı değer get ediliyor
             return View();
         }
 
